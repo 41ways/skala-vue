39f3b6bf9db18a9d17a831089f28d0e01a044168
정한결 <gyolnoy@gmail.com>
2026-08-19 16:01:24 +0900

    실습: v-pre, v-cloak, v-once, v-memo 렌더링 제어

---
+++ src/components/practices/directive/VCloakBasic.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('느린 네트워크에서도 안전하게 출력되는 메시지!')
+</script>
+
+<template>
+  <div v-cloak class="practice-section">
+    <h2>v-cloak 디렉티브 학습</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+/* Vue가 로딩되기 전까지 이 구역을 숨김. 이 CSS가 없으면 v-cloak은 아무 일도 안 함 */
+[v-cloak] {
+  display: none !important;
+}
+</style>
