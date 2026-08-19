6ddf15daf3cfb88143f12c144721640f5e54dfbd
정한결 <gyolnoy@gmail.com>
2026-08-19 16:07:07 +0900

    실습: 이벤트 객체와 $event 명시 전달

---
+++ src/components/practices/event/EventObject.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const position = ref('')
+const tagName = ref('')
+
+// 함수 이름만 넘기면 이벤트 객체가 첫 인자로 자동으로 들어옴
+const getOnlyEvent = (e) => {
+  position.value = `좌표: X=${e.clientX}, Y=${e.clientY}`
+}
+
+// 데이터를 같이 넘길 때는 $event를 명시해야 이벤트 객체가 따라옴
+const getWithParam = (name, e) => {
+  tagName.value = `대상: ${name} / 클릭된 태그: ${e.target.tagName}`
+}
+</script>
+
+<template>
+  <div class="practice-section">
+    <h2>v-on 이벤트 객체($event) 활용</h2>
+
+    <p>좌표: {{ position }}</p>
+    <p>태그: {{ tagName }}</p>
+
+    <button @click="getOnlyEvent">클릭 좌표 알아내기</button>
+    <button @click="getWithParam('회원A', $event)">회원 정보와 태그 확인</button>
+  </div>
+</template>
