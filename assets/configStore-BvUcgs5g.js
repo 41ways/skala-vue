ef9b79982b3f2daa580e53a7e931f02d61a1b240
41ways <41ways@users.noreply.github.com>
2026-08-22 09:05:26 +0000

    deploy: d2667167dd62c9ce354a3a4db113433b90f2207c

--- assets/configStore-BvUcgs5g.js
+++
@@ -1,1 +0,0 @@
-import{R as e,b as t,h as n}from"./index-9rIKKJjM.js";var r=n(`config`,()=>{let n=e(`celsius`),r=e(``);return{unit:n,sortOrder:r,unitSymbol:t(()=>n.value===`celsius`?`℃`:`℉`),toggleUnit:()=>{n.value=n.value===`celsius`?`fahrenheit`:`celsius`},toTemp:e=>n.value===`fahrenheit`?Math.round(e*9/5+32):e,changeSort:e=>{r.value=r.value===e?``:e}}});export{r as t};
